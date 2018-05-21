dfc53dea888103d1b7a40e7c00ef07e879fc3fc2
Evelina <evelinabrinkemo@gmail.com>
2018-05-22 00:54:51 +0200

    draggable editor window

--- app.js
+++ app.js
@@ -1,6 +1,25 @@
 let textarea = document.querySelector('textarea');
 let previewer = document.querySelector('#previewer');
+let editorHeader = document.querySelector('#editorHeader');
+let editorDiv = document.querySelector('#editor');
+
+let dragging = false;
 
 textarea.addEventListener('input', function (event) {
     previewer.innerHTML = marked(event.target.value);
 });
+
+editorHeader.addEventListener('mousedown', function (event) {
+    dragging = true;
+})
+
+window.addEventListener('mouseup', function (){
+    dragging = false;
+})
+
+window.addEventListener('mousemove', function (event) {
+    if (dragging) {
+        editorDiv.style.left = event.clientX + 'px';
+        editorDiv.style.top = event.clientY + 'px';
+    }
+})
