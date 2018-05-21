a72843666939c1a6833afc93b56b02ec936ad3d6
Evelina <evelinabrinkemo@gmail.com>
2018-05-21 17:46:47 +0200

    added marked library

--- app.js
+++ app.js
@@ -2,6 +2,5 @@ let textarea = document.querySelector('textarea');
 let previewer = document.querySelector('#previewer');
 
 textarea.addEventListener('input', function (event) {
-    let text = textarea.value;
-    previewer.textContent = text;
+    previewer.innerHTML = marked(event.target.value);
 });
