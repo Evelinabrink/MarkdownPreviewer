59c51df3397f2bbf1c4e721131db72dea24a715a
Evelina <evelinabrinkemo@gmail.com>
2018-05-22 19:48:46 +0200

    added drag and drop for both windows

--- app.js
+++ app.js
@@ -2,24 +2,49 @@ let textarea = document.querySelector('textarea');
 let previewer = document.querySelector('#previewer');
 let editorHeader = document.querySelector('#editorHeader');
 let editorDiv = document.querySelector('#editor');
+let previewHeader = document.querySelector('#previewHeader');
+let previewDiv = document.querySelector('#previewDiv');
 
-let dragging = false;
+let dragEd = false;
+let dragPreview = false;
 
 textarea.addEventListener('input', function (event) {
     previewer.innerHTML = marked(event.target.value);
 });
 
+// drag and drop the editor window
 editorHeader.addEventListener('mousedown', function (event) {
-    dragging = true;
+    dragEd = true;
+    editorDiv.style.zIndex = 1;
 })
 
 window.addEventListener('mouseup', function (){
-    dragging = false;
+    dragEd = false;
+    editorDiv.style.zIndex = 0;
 })
 
 window.addEventListener('mousemove', function (event) {
-    if (dragging) {
+    if (dragEd) {
         editorDiv.style.left = event.clientX + 'px';
         editorDiv.style.top = event.clientY + 'px';
+    }
+})
+
+
+// drag and drop the preview window
+previewHeader.addEventListener('mousedown', function (event) {
+    dragPreview = true;
+    previewDiv.style.zIndex = 1;
+})
+
+window.addEventListener('mouseup', function (){
+    dragPreview = false;
+    previewDiv.style.zIndex = 0;
+})
+
+window.addEventListener('mousemove', function (event) {
+    if (dragPreview) {
+        previewDiv.style.left = event.clientX + 'px';
+        previewDiv.style.top = event.clientY + 'px';
     }
 })
